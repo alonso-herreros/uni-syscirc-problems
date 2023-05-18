25c68837985fb0f0abbc46f4417b1f1565863d67
Alonso Herreros <alonso.herreros.c@gmail.com>
2023-05-18 20:21:55 +0200

    Change error type when failing evaluate_on_range

--- code/custom_libs/syscirc/syscirc/symbolics.py
+++ code/custom_libs/syscirc/syscirc/symbolics.py
@@ -77,8 +77,8 @@ def evaluate_on_range(f, time:list[float], var:Symbol=Symbol("t"), **kwargs):
 
         try: val = float(Known.resolve(val, levels=-1, evalf=True))
         except TypeError as e:
-            try: raise TypeError(f"Could not evaluate {val} to float. Symbols left: {val.free_symbols}.") from e
-            except TypeError: raise TypeError(f"Could not evaluate {val} to float.") from e
+            try: raise ValueError(f"Could not evaluate {val} to float. Symbols left: {val.free_symbols}.") from e
+            except TypeError: raise ValueError(f"Could not evaluate {val} to float.") from e
 
 
         values.append(val if val is not None and val != nan else 0)
